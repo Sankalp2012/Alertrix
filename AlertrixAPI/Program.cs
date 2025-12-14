259f268765f64dc4e45b4fb5e1a11abc80a1901c
Sankalp2012 <sankalpgarg2012@gmail.com>
2025-12-14 22:12:42 +0530

    remove swagger from dev env

--- AlertrixAPI/Program.cs
+++ AlertrixAPI/Program.cs
@@ -76,11 +76,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
